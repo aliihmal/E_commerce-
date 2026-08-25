683a0cd7d7fccb792624b8b7f9ed1372efe33fc3
Ali hamza <alihamza637376@gmail.com>
2026-08-25 12:21:42 +0300

    fixed the authentication

--- setup/src/middleware/auth.ts
+++ setup/src/middleware/auth.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { AuthenticatinService } from "../services/Auth.service";
-import { AuthenticationFailed } from "../util/Exception/http/Authenticationexception";
+import { AuthenticationFailed, TokenExepiredException } from "../util/Exception/http/Authenticationexception";
 import { AuthReq } from "../config/type";
 
 
@@ -9,21 +9,23 @@ import { AuthReq } from "../config/type";
 const authService = new AuthenticatinService();
 
 
-export function authenticate(req:Request,res:Response,next:NextFunction){
-    let token =req.cookies.token;
-    const refreshToken= req.cookies.refreshToken;
+export function authenticate(req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies.token;
+    const refreshToken = req.cookies.refreshToken;
 
-    if(!token){
-        if(!refreshToken){
+    try {
+        if (!token) throw new TokenExepiredException(); // no token = treat like expired, go refresh
+        const payload = authService.verirfyToken(token);
+        (req as unknown as AuthReq).user = payload;
+        return next();
+    } catch (err) {
+        if (!(err instanceof TokenExepiredException) || !refreshToken) {
             throw new AuthenticationFailed();
         }
-            const newToken=authService.refreshToken(refreshToken);
-            authService.setTokenIntocookie(res,newToken);
-        
-       
-            token=newToken;
+        const newToken = authService.refreshToken(refreshToken); // will throw if refresh token is bad/expired too
+        authService.setTokenIntocookie(res, newToken);
+        const payload = authService.verirfyToken(newToken);
+        (req as unknown as AuthReq).user = payload;
+        return next();
     }
-    const payload = authService.verirfyToken(token);
-    (req as unknown as AuthReq).user = payload;
-    next(); 
 }
